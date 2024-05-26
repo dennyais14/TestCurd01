7770e48db9f20125966f1bdcbd19a3f0a44f162e
danieleocchi <dennyais146@gmail.com>
2024-05-26 20:08:32 +0200

    second commit

--- src/main/java/co/develhope/TestCrud01/repositories/StudentRepository.java
+++ src/main/java/co/develhope/TestCrud01/repositories/StudentRepository.java
@@ -2,6 +2,8 @@ package co.develhope.TestCrud01.repositories;
 
 import co.develhope.TestCrud01.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
